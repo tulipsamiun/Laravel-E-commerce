f1590e71ecca6528ad8d0f12b6b66c26e8fc3df2
Tulip Samiun <tsamiun_ts@yahoo.com>
2022-02-26 01:51:20 +0600

    Login Done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash ;
+
+
+class UserController extends Controller
+{
+    //
+    function login(Request $req)
+    {
+        $user =  User:: where (['email'=>$req->email])->first();
+        if(!$user || !Hash::check($req->password, $user->password))
+        {
+            return "User Name Or Password Not Match";
+        }
+        else
+        {
+            $req->session()->put('user',$user);
+            return redirect('/');
+        }
+    }
+}
---
+++ resources/views/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>   
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
+  <!-- <script>
+       $(document).ready(function(){
+           $("button").click(function(){
+               alert("all set")
+           })
+       })
+   </script>--> Hello New Page
+</body>
+<style>
+    .custom-login{
+        height: 500px;
+        padding-top: 100px;
+    }}
+</style>
+</html>
